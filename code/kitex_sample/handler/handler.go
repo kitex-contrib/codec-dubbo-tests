e077823c113db5d4447e4a80f9835c86438e481c
whalecold <zls1129@gmail.com>
2024-05-21 11:36:05 +0800

    remove useless code

--- code/kitex_sample/handler/handler.go
+++ code/kitex_sample/handler/handler.go
@@ -21,7 +21,6 @@ package handler
 
 import (
 	"context"
-	"time"
 
 	"github.com/kitex-contrib/codec-dubbo-tests/code/kitex_sample/kitex_gen/hello"
 )
@@ -31,12 +30,10 @@ type GreetServiceImpl struct{}
 
 // Greet implements the GreetServiceImpl interface.
 func (s *GreetServiceImpl) Greet(ctx context.Context, req string) (resp string, err error) {
-	time.Sleep(time.Millisecond * 10)
 	return "Hello " + req, nil
 }
 
 // GreetWithStruct implements the GreetServiceImpl interface.
 func (s *GreetServiceImpl) GreetWithStruct(ctx context.Context, req *hello.GreetRequest) (resp *hello.GreetResponse, err error) {
-	time.Sleep(time.Millisecond * 10)
 	return &hello.GreetResponse{Resp: "Hello " + req.Req}, nil
 }
